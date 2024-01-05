bd9d777217d078ce4ffd0fe2e07fa82452877d56
colinmcnabb <mcnabb.colin.lee@gmail.com>
2024-01-05 18:44:26 +0000

    update dev environment

--- pull_data.py
+++ pull_data.py
@@ -3,21 +3,23 @@ import requests
 import os
 from dotenv import load_dotenv
 
-load_dotenv() 
+load_dotenv()
 
-api_key = os.getenv('API_KEY')
+api_key = os.getenv("API_KEY")
 print(api_key)
 
-url = 'https://api.collegefootballdata.com/games'
-headers = {'Accept': 'application/json', 'Authorization': f'Bearer {api_key}'}
-params = {'year': 2022, 'seasonType': 'regular'}
+url = "https://api.collegefootballdata.com/games"
+headers = {"Accept": "application/json", "Authorization": f"Bearer {api_key}"}
+params = {"year": 2022, "seasonType": "regular"}
 response = requests.get(url, headers=headers, params=params)
 print(response.status_code)
 # df = pd.read_json(response.text)
-df = pd.json_normalize(response.json()) # dataframe, json = java script object notation (file format)
-df.columns = map(str.upper, df.columns) # change all the columns title to upper case letters, makes easier for SQL
+df = pd.json_normalize(
+    response.json()
+)  # dataframe, json = java script object notation (file format)
+df.columns = map(
+    str.upper, df.columns
+)  # change all the columns title to upper case letters, makes easier for SQL
 
-print(df.head()) # print first 5 rows
-df.to_csv('games.csv', index = False)
-
-
+print(df.head())  # print first 5 rows
+df.to_csv("games.csv", index=False)
